cc4e24b22fd489c8710f250981546ed27bf8de96
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-05-30 01:29:19 -0500

    add login and register

--- Sources/ibl-mobile-extensions-ios /ibl_mobile_extensions_ios_.swift
+++ Sources/ibl-mobile-extensions-ios /ibl_mobile_extensions_ios_.swift
@@ -1,6 +1,81 @@
-public struct ibl_mobile_extensions_ios_ {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+class IBLWebAppAuth {
+    
+    static func login(server: String, username: String, password: String) {
+        let url = URL(string: server + "/wp-json/ibl-wp-platform/v1/mentor-sign-in")!
+        var request = URLRequest(url: url)
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let encoder = JSONEncoder()
+        let message = [
+            "username": username,
+            "password": password
+        ]
+        
+        do {
+            let data = try encoder.encode(message)
+            request.httpBody = data
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+                
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(dataString)")
+                }
+            }
+            task.resume()
+        }
+        catch {
+            print("Could not send POST to web edx")
+        }
+    }
+    
+    
+    static func register(server: String, email: String, username: String, password: String) {
+        let url = URL(string: server + "/wp-json/ibl-wp-platform/v1/mentor-sign-up")!
+        var request = URLRequest(url: url)
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let encoder = JSONEncoder()
+        let message = [
+            "username": username,
+            "name": username,
+            "email": email,
+            "password": password
+        ]
+        
+        do {
+            let data = try encoder.encode(message)
+            request.httpBody = data
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+                
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(dataString)")
+                }
+            }
+            task.resume()
+        }
+        catch {
+            print("Could not send POST to web edx")
+        }
     }
 }
